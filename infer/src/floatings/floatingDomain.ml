81a8c69c1694ee47bad35154ab7c3d2b16c8bf83
Michele Miccinesi <michele.miccinesi@gmail.com>
2019-05-22 12:07:59 +0200

    bugs...

--- infer/src/floatings/floatingDomain.ml
+++ infer/src/floatings/floatingDomain.ml
@@ -5,72 +5,72 @@ module F = Format
 
 (* The following min, max, eq functions cope with the sign of 0. and with NaN *)
 let min_nan (a:float) (b:float) : float = 
-	match (classify_float a, classify_float b) with
-	| (FP_nan, _) | (_, FP_nan) -> nan
-	| (FP_zero, FP_zero) -> if (min (copysign 1. a) (copysign 1. b))=(-1.) then (-0.) else 0.
-	| _ -> min a b
+  match (classify_float a, classify_float b) with
+  | (FP_nan, _) | (_, FP_nan) -> nan
+  | (FP_zero, FP_zero) -> if (min (copysign 1. a) (copysign 1. b))=(-1.) then (-0.) else 0.
+  | _ -> min a b
 
 let max_nan (a:float) (b:float) : float = 
-	match (classify_float a, classify_float b) with
-	| (FP_nan, _) | (_, FP_nan) -> nan
-	| (FP_zero, FP_zero) -> if (max (copysign 1. a) (copysign 1. b))=1. then 0. else (-0.)
-	| _ -> max a b
+  match (classify_float a, classify_float b) with
+  | (FP_nan, _) | (_, FP_nan) -> nan
+  | (FP_zero, FP_zero) -> if (max (copysign 1. a) (copysign 1. b))=1. then 0. else (-0.)
+  | _ -> max a b
 
 let eq_nan (a:float) (b:float) : bool =
-	match (classify_float a, classify_float b) with
-	| (FP_nan, x) | (x, FP_nan) -> x=FP_nan 
-	| (FP_zero, FP_zero) -> (copysign 1. a) = (copysign 1. b)
-	| _ -> a=b
+  match (classify_float a, classify_float b) with
+  | (FP_nan, x) | (x, FP_nan) -> x=FP_nan 
+  | (FP_zero, FP_zero) -> (copysign 1. a) = (copysign 1. b)
+  | _ -> a=b
 
 module Range_el = struct
-	type t = 
-	| Range of float*float
-	| Bottom
+  type t = 
+  | Range of float*float
+  | Bottom
 
-	let ( <= ) (lhs:t) (rhs:t) : bool = 
-		match (lhs, rhs) with
-		| (Bottom, _)		-> true
-		| (Range _, Bottom)	-> false
-		| (Range (lhs_l, lhs_u), Range (rhs_l, rhs_u))	
-			-> (eq_nan (min_nan lhs_l rhs_l) rhs_l) && (eq_nan (max_nan lhs_u rhs_u) rhs_u)
-	
-	let merge (a:t) (b:t) : t =
-		match (a,b) with
-		| (Bottom, x) | (x, Bottom) -> x
-		| (Range (a_l, a_u), Range (b_l, b_u)) -> Range (min_nan a_l b_l, max_nan a_u b_u)
-	(* !!! XƏBƏRDARLIQ: min/max is now considering NaN !!! *)	
+  let ( <= ) (lhs:t) (rhs:t) : bool = 
+    match (lhs, rhs) with
+    | (Bottom, _)   -> true
+    | (Range _, Bottom) -> false
+    | (Range (lhs_l, lhs_u), Range (rhs_l, rhs_u))  
+      -> (eq_nan (min_nan lhs_l rhs_l) rhs_l) && (eq_nan (max_nan lhs_u rhs_u) rhs_u)
+  
+  let merge (a:t) (b:t) : t =
+    match (a,b) with
+    | (Bottom, x) | (x, Bottom) -> x
+    | (Range (a_l, a_u), Range (b_l, b_u)) -> Range (min_nan a_l b_l, max_nan a_u b_u)
+  (* !!! XƏBƏRDARLIQ: min/max is now considering NaN !!! *) 
 
-	let constrain (base:t) (constr:t) : t =
-		match (base,constr) with
-		| (Bottom, _) | (_, Bottom) -> Bottom
-		| (Range (a_l, a_u), Range (b_l, b_u)) -> 
-			let canonic_range (rng:t) =
-				match rng with
-				| Range (l,u) when eq_nan (min_nan l u) l -> rng
-				| _ -> Bottom
-			in canonic_range (Range (max a_l b_l, min a_u b_u))
+  let constrain (base:t) (constr:t) : t =
+    match (base,constr) with
+    | (Bottom, _) | (_, Bottom) -> Bottom
+    | (Range (a_l, a_u), Range (b_l, b_u)) -> 
+      let canonic_range (rng:t) =
+        match rng with
+        | Range (l,u) when eq_nan (min_nan l u) l -> rng
+        | _ -> Bottom
+      in canonic_range (Range (max a_l b_l, min a_u b_u))
 end
 
 module Range_el_opt = struct
-	type t = Range_el.t option
+  type t = Range_el.t option
 
-	let ( <= ) (lhs:t) (rhs:t) : bool =
-		match (lhs, rhs) with
-		| (None, _) -> true
-		| (Some l, Some r) -> Range_el.(l <= r)
-		| (_, None) -> false
-	
-	let merge (a:t) (b:t) : t =
-		match (a,b) with
-		| (None, b) -> b
-		| (a, None) -> a
-		| (Some a', Some b') -> Some (Range_el.merge a' b')
+  let ( <= ) (lhs:t) (rhs:t) : bool =
+    match (lhs, rhs) with
+    | (None, _) -> true
+    | (Some l, Some r) -> Range_el.(l <= r)
+    | (_, None) -> false
+  
+  let merge (a:t) (b:t) : t =
+    match (a,b) with
+    | (None, b) -> b
+    | (a, None) -> a
+    | (Some a', Some b') -> Some (Range_el.merge a' b')
 
-	let constrain (base:t) (constr:t) : t =
-		match (base,constr) with
-		| (None,constr) -> None
-		| (a,None) -> a
-		| (Some base',Some constr') -> Some (Range_el.constrain base' constr')
+  let constrain (base:t) (constr:t) : t =
+    match (base,constr) with
+    | (None,constr) -> None
+    | (a,None) -> a
+    | (Some base',Some constr') -> Some (Range_el.constrain base' constr')
 end
 
 (* (Ocaml) -- First steps to make this parametric... **)
@@ -81,18 +81,18 @@ module Value_opt = Range_el_opt
 type t = (key, Value.t) Hashtbl.t
 
 let ( <= ) ~lhs ~rhs = 
-	let cmp (k:key) (v:Value.t) (cum:bool) =
-		cum && Value_opt.((Some v) <= Hashtbl.find_opt rhs k)
-	in Hashtbl.fold cmp lhs true
+  let cmp (k:key) (v:Value.t) (cum:bool) =
+    cum && Value_opt.((Some v) <= Hashtbl.find_opt rhs k)
+  in Hashtbl.fold cmp lhs true
 
 let combine (a:t) (b:t) ~combiner:combiner : t =
-	let combine_els (k:key) (v:Value.t) (cum:t) =
-		let replace_opt (tbl:t) (k:key) (v_opt:Value_opt.t) =
-			match v_opt with
-			| None -> tbl
-			| Some v' -> Hashtbl.replace tbl k v'; tbl
-		in replace_opt cum k (combiner (Some v) (Hashtbl.find_opt cum k))
-	in Hashtbl.fold combine_els a b
+  let combine_els (k:key) (v:Value.t) (cum:t) =
+    let replace_opt (tbl:t) (k:key) (v_opt:Value_opt.t) =
+      match v_opt with
+      | None -> tbl
+      | Some v' -> Hashtbl.replace tbl k v'; tbl
+    in replace_opt cum k (combiner (Some v) (Hashtbl.find_opt cum k))
+  in Hashtbl.fold combine_els a b
 
 let join = combine ~combiner:Value_opt.merge
 
@@ -103,9 +103,9 @@ let constrain = combine ~combiner:Value_opt.constrain
 let widening_threshold = 5
 
 let widen ~prev ~next ~num_iters = 
-	if num_iters<widening_threshold 
-		then join prev next
-	else next
+  if num_iters<widening_threshold 
+    then join prev next
+  else next
 
 let pp fmt () = ()
 
@@ -115,20 +115,20 @@ type summary = t
 
 (*
 let join (a:t) (b:t) : t = 
-	let merge_els (str:string) (rng:Range_el.t) (cum:t) =
-		let replace_opt (tbl:t) (str:string) (rng_opt:Range_el_opt.t) =
-			match rng_opt with
-			| None -> tbl
-			| Some rng' -> Hashtbl.replace tbl str rng'; tbl
-		in replace_opt cum str (Range_el_opt.merge (Some rng) (Hashtbl.find_opt cum str))
-	in Hashtbl.fold merge_els a b
+  let merge_els (str:string) (rng:Range_el.t) (cum:t) =
+    let replace_opt (tbl:t) (str:string) (rng_opt:Range_el_opt.t) =
+      match rng_opt with
+      | None -> tbl
+      | Some rng' -> Hashtbl.replace tbl str rng'; tbl
+    in replace_opt cum str (Range_el_opt.merge (Some rng) (Hashtbl.find_opt cum str))
+  in Hashtbl.fold merge_els a b
 
 let constrain (bases:t) (constrs:t) : t = 
-	let constrain_els (str:string) (rng:Range_el.t) (cum:t) =
-		let replace_opt (tbl:t) (str:string) (rng_opt:Range_el_opt.t) =
-			match rng_opt with
-			| None -> tbl
-			| Some rng' -> Hashtbl.replace tbl str rng'; tbl
-		in replace_opt cum str (Range_el_opt.constrain (Some rng) (Hashtbl.find_opt cum str))
-	in Hashtbl.fold merge_els a b
+  let constrain_els (str:string) (rng:Range_el.t) (cum:t) =
+    let replace_opt (tbl:t) (str:string) (rng_opt:Range_el_opt.t) =
+      match rng_opt with
+      | None -> tbl
+      | Some rng' -> Hashtbl.replace tbl str rng'; tbl
+    in replace_opt cum str (Range_el_opt.constrain (Some rng) (Hashtbl.find_opt cum str))
+  in Hashtbl.fold merge_els a b
 **)
--- infer/src/floatings/floatings.ml
+++ infer/src/floatings/floatings.ml
@@ -5,33 +5,33 @@ module F = Format
 module L = Logging
 
 module Payload = SummaryPayload.Make (struct
-	type t = FloatingDomain.t
+  type t = FloatingDomain.t
 
-	let update_payloads resources_payload (payloads : Payloads.t) =
-		{payloads with }
+  let update_payloads resources_payload (payloads : Payloads.t) =
+    {payloads with }
 
-	let of_payloads {Payloads.} = 
+  let of_payloads {Payloads.} = 
 end)
 
 module TransferFunctions (CFG : ProcCfg.S) = struct
-	module CFG = CFG
-	module Domain = FloatingDomain
+  module CFG = CFG
+  module Domain = FloatingDomain
 
-	type extras = ProcData.no_extras
+  type extras = ProcData.no_extras
 
-	(* val exec_instr : t -> Sil.instr -> Procdesc.Node.nodekind -> Typ.Procname.t -> Tenv.t -> t
-	   input is the previous state, current instruction, current node kind, current procedure and
+  (* val exec_instr : t -> Sil.instr -> Procdesc.Node.nodekind -> Typ.Procname.t -> Tenv.t -> t
+     input is the previous state, current instruction, current node kind, current procedure and
       type environment.
-	**)
-	(* Domain.t -> extras ProcData.t -> CFG.Node.t -> instr -> Domain.t
-	astate _ _ instr
-	type 'a t = {pdesc: Procdesc.t; tenv: Tenv.t; extras: 'a}  **)
-	let exec_instr (astate : FoatingDomain.t) {ProcData.pdesc= _; tenv= _} node instr = 
-		match instr with
-		| Sil.Load
-		| Sil.Store
+  **)
+  (* Domain.t -> extras ProcData.t -> CFG.Node.t -> instr -> Domain.t
+  astate _ _ instr
+  type 'a t = {pdesc: Procdesc.t; tenv: Tenv.t; extras: 'a}  **)
+  let exec_instr (astate : FoatingDomain.t) {ProcData.pdesc= _; tenv= _} node instr = 
+    match instr with
+    | Sil.Load
+    | Sil.Store
 
-	let pp_session_name _node fmt = F.pp_print_string fmt "Floatings checker"
+  let pp_session_name _node fmt = F.pp_print_string fmt "Floatings checker"
 end
 (* Naive useless approach, just to check the framework interface! **)
 module CFG = ProcCfg.(OneInstrPerNode (Normal))
@@ -43,15 +43,15 @@ module Analyzer = AbstractInterpreter.MakeWTO (TransferFunctions (CFG))
 
 (* Register in RegisterCheckers *)
 let checker {Callbacks.summary; proc_desc; tenv} : Summary.t =
-	let proc_data = ProcData.make proc_desc tenv () in
-	match Analyzer.compute_post proc_data ~initial:FloatingDomain.initial with
-	| Some post ->
-		report_if_NaR post summary proc_data ;
-		Payload.update_summary post summary
-	| None ->
-		L.(die InternalError)
-			"Analyzer failed to compute post for %a" Typ.Procname.pp
-			(Procdesc.get_proc_name proc_data.pdesc)
+  let proc_data = ProcData.make proc_desc tenv () in
+  match Analyzer.compute_post proc_data ~initial:FloatingDomain.initial with
+  | Some post ->
+    report_if_NaR post summary proc_data ;
+    Payload.update_summary post summary
+  | None ->
+    L.(die InternalError)
+      "Analyzer failed to compute post for %a" Typ.Procname.pp
+      (Procdesc.get_proc_name proc_data.pdesc)
 
 
 (* module CFG = ProcCfg.Normal **)
@@ -61,10 +61,10 @@ module CheckerAnalyzer = AbstractInterpreter.MakeRPO (TransferFunctions (Checker
 module CapturedByRefAnalyzer =
   AbstractInterpreter.MakeRPO (CapturedByRefTransferFunctions (ProcCfg.Exceptional))
 **)
-		(* (instr : HilInstr.t) = *)
-		(* /IR/HilInstr.ml[i] *)
-		(* match instr with
-		| Assign (access_expr, AccessExpression rhs_access_expr, _loc) ->
+    (* (instr : HilInstr.t) = *)
+    (* /IR/HilInstr.ml[i] *)
+    (* match instr with
+    | Assign (access_expr, AccessExpression rhs_access_expr, _loc) ->
         ResourceLeakDomain.assign
           (HilExp.AccessExpression.to_access_path access_expr)
           (HilExp.AccessExpression.to_access_path rhs_access_expr)
